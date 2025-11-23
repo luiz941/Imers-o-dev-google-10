e4de5d6a6321fc8997a1e519e99b30f1616e79a3
draken <luizotavio225867@gmail.com>
2025-11-22 22:28:43 -0300

    busca e cards
    adição da funcionalidade do botão de busca, adição da funcionalidade da barra de busca

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
+let cardContainer = document.querySelector("main");
+let campoBusca = document.querySelector("header input");
 let dados = [];
 
 async function iniciarBusca() {
-    let resposta = await fetch("data.json");
-    dados = await resposta.json();
-    console.log(dados);
+    if (dados.length === 0) {
+        try {
+            let resposta = await fetch("data.json");
+            dados = await resposta.json();
+        } catch (erro) {
+            console.error("Falha ao buscar dados: ", erro);
+            return;
+        }
+    }
+
+    const termoBusca = campoBusca.value.toLowerCase();
+    const dadosFiltrados = dados.filter(dado =>
+        dado.nome.toLowerCase().includes(termoBusca)||
+        dado.descrição.toLowerCase().includes(termoBusca)
+    );
+
+    renderizarCards(dadosFiltrados);
+}
+
+function renderizarCards(dados) {
+    cardContainer.innerHTML = "";
+    for (let dado of dados) {
+        let article = document.createElement("article");
+        article.innerHTML = `
+        <h2>${dado.nome}</h2>
+        <p><strong>Ano de lançamento:</strong> ${dado.ano}</p>
+        <p>${dado.descrição}</p>
+        <a href="${dado.link}" target="_blank">Saiba mais</a>
+        `;
+        cardContainer.appendChild(article);
+    }
 }
